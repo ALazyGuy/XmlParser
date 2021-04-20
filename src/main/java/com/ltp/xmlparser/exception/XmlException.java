c6043c77bbaabb5d7f2a92c90bcdadaf2056426f
ALazyGuy <razorprogrammer@gmail.com>
2021-04-20 11:01:39 +0300

    feat: Added XMLException

---
+++ src/main/java/com/ltp/xmlparser/exception/XmlException.java
@@ -0,0 +1,21 @@
+package com.ltp.xmlparser.exception;
+
+public class XmlException extends Exception{
+
+    public XmlException() {
+        super();
+    }
+
+    public XmlException(String message) {
+        super(message);
+    }
+
+    public XmlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public XmlException(Throwable cause) {
+        super(cause);
+    }
+    
+}
